d53bcb8146c2cdba8e40d2a25a82e8fe67de61c1
BettyM <betty.murillo@gorillanation.com>
2013-09-13 16:08:00 -0700

    Adding list template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import views._
+import models._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def list() = Action {
+      val movies = Movie.findAll
+      Ok(html.list(movies))
   }
 }
