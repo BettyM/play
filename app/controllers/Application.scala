13f978c45ecd382fced07cecbeff0518f43e44c5
BettyM <betty.murillo@gorillanation.com>
2013-09-16 11:48:22 -0700

    New route to details.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,11 +4,20 @@ import play.api._
 import play.api.mvc._
 import views._
 import models._
+import se.radley.plugin.salat._
+import com.mongodb.casbah.Imports._
+import com.novus.salat._
 
 object Application extends Controller {
 
   def list() = Action {
       val movies = Movie.findAll
       Ok(html.list(movies))
+  }
+
+  def view(id: ObjectId) = Action {
+      Movie.findOneById(id).map( movie =>
+        Ok(html.movie(movie))
+      ).getOrElse(NotFound)
   }
 }
--- app/models/Movie.scala
+++ app/models/Movie.scala
@@ -1,5 +1,6 @@
 package models
 
+import play.api.Play.current
 import java.util.Date
 import com.novus.salat._
 //import com.novus.salat.global._
@@ -7,13 +8,15 @@ import com.novus.salat.annotations._
 import com.novus.salat.dao._
 //import com.novus.salat.dao.{ SalatDAO, ModelCompanion }
 import com.mongodb.casbah.Imports._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 import se.radley.plugin.salat._
 import se.radley.plugin.salat.Binders._
 import mongoContext._
 
 case class Movie (
-              @Key("_id") id: ObjectId = new ObjectId,
+              @Key("_id")id: ObjectId = new ObjectId,
               title: String,
               genre: String,
               description: String,
@@ -26,7 +29,7 @@ case class Movie (
 object Movie extends MovieDAO
 
 trait MovieDAO extends ModelCompanion[Movie, ObjectId] {
-  val collection = MongoConnection()("playdb")("movies")
+  def collection = mongoCollection("movies")
   val dao = new SalatDAO[Movie, ObjectId](collection) {}
 
   //Queries
